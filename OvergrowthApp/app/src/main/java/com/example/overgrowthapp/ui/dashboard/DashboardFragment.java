b588493bc155aef66d33865788a3d7cdbf438e03
ddnold <s532534@nwmissouri.edu>
2023-03-10 02:25:00 -0600

    fixed cardview display and added scrollview with query system
    ScrollView is optimizing the plant recyclerView at bit. Definitely still needs some work getting the speed up but a big improvement.

--- OvergrowthApp/app/src/main/java/com/example/overgrowthapp/ui/dashboard/DashboardFragment.java
+++ OvergrowthApp/app/src/main/java/com/example/overgrowthapp/ui/dashboard/DashboardFragment.java
@@ -18,6 +18,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -33,6 +34,7 @@ public class DashboardFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_dashboard,container,false);
         recyclerView = view.findViewById(R.id.plantList);
         database = FirebaseDatabase.getInstance().getReference();
+        Query query = database.orderByChild("index").limitToFirst(10);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext());
         recyclerView.setLayoutManager(linearLayoutManager);
@@ -40,28 +42,63 @@ public class DashboardFragment extends Fragment {
         list = new ArrayList<>();
         myAdapter = new myAdapter(view.getContext(),list);
         recyclerView.setAdapter(myAdapter);
-        database.addValueEventListener(new ValueEventListener() {
+        query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()){
 
                     user User = dataSnapshot.getValue(user.class);
                     list.add(User);
+
                 }
                 myAdapter.notifyDataSetChanged();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
 
+                // Check if the last item is about to be shown
+                if (!recyclerView.canScrollVertically(1)) {
+                    // Get the last item in the list to use as the starting point for the next query
+                    user lastUser = list.get(list.size() - 1);
+                    String lastUserId = lastUser.getIndex();
+
+                    // Update the query to fetch the next 10 items
+                    Query nextQuery = database.orderByChild("index").startAfter(lastUserId).limitToFirst(10);
+                    nextQuery.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            int sizeBefore = list.size();
+
+                            for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                                user User = dataSnapshot.getValue(user.class);
+                                list.add(User);
+
+                            }
+
+                            int sizeAfter = list.size();
+                            myAdapter.notifyItemRangeInserted(sizeBefore, sizeAfter - sizeBefore);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                        }
+                    });
+                }
             }
         });
         return view;
     }
 
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        recyclerView = null;
     }
 }
--- OvergrowthApp/app/src/main/java/com/example/overgrowthapp/ui/dashboard/user.java
+++ OvergrowthApp/app/src/main/java/com/example/overgrowthapp/ui/dashboard/user.java
@@ -2,7 +2,11 @@ package com.example.overgrowthapp.ui.dashboard;
 
 public class user {
 
-    String CommonId, BotanicalID;
+    String CommonId, BotanicalID, index;
+
+    public String getIndex() {
+        return index;
+    }
 
     public String getCommonID() {
         return CommonId;
